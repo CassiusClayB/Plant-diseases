b4c9d8ac5d356b75ec2ab496e7ebe97ca4245620
Cassius Clay Filho <cassiussilvafilho@gmail.com>
2025-04-21 12:43:23 -0300

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,88 @@
+from flask import Flask, request, render_template
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import time
+import numpy as np
+import os
+
+app = Flask(__name__)
+model = load_model('app/model/melhor_modelo.keras')
+classes = [
+    'Apple___Apple_scab',
+    'Apple___Black_rot',
+    'Apple___Cedar_apple_rust',
+    'Apple___healthy',
+    'Blueberry___healthy',
+    'Cherry_(including_sour)___Powdery_mildew',
+    'Cherry_(including_sour)___healthy',
+    'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+    'Corn_(maize)___Common_rust_',
+    'Corn_(maize)___Northern_Leaf_Blight',
+    'Corn_(maize)___healthy',
+    'Grape___Black_rot',
+    'Grape___Esca_(Black_Measles)',
+    'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+    'Grape___healthy',
+    'Orange___Haunglongbing_(Citrus_greening)',
+    'Peach___Bacterial_spot',
+    'Peach___healthy',
+    'Pepper,_bell___Bacterial_spot',
+    'Pepper,_bell___healthy',
+    'Potato___Early_blight',
+    'Potato___Late_blight',
+    'Potato___healthy',
+    'Raspberry___healthy',
+    'Soybean___healthy',
+    'Squash___Powdery_mildew',
+    'Strawberry___Leaf_scorch',
+    'Strawberry___healthy',
+    'Tomato___Bacterial_spot',
+    'Tomato___Early_blight',
+    'Tomato___Late_blight',
+    'Tomato___Leaf_Mold',
+    'Tomato___Septoria_leaf_spot',
+    'Tomato___Spider_mites Two-spotted_spider_mite',
+    'Tomato___Target_Spot',
+    'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+    'Tomato___Tomato_mosaic_virus',
+    'Tomato___healthy'
+]
+
+
+
+def preprocess(img_path):
+    img = image.load_img(img_path, target_size=(128, 128))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x /= 255.
+    return x
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['image']
+        path = os.path.join('app/static', file.filename)
+        file.save(path)
+
+        img = preprocess(path)
+        
+        start = time.time()
+        pred = model.predict(img)
+        end = time.time()
+
+        predicted_index = np.argmax(pred)
+        label = classes[predicted_index]
+        confidence = round(float(pred[0][predicted_index]) * 100, 2)
+        prediction_time = round(end - start, 4)
+
+        return render_template('index.html',
+                               prediction=label,
+                               confidence=confidence,
+                               prediction_time=prediction_time,
+                               image_path=path)
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
